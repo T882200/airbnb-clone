e37498f166a6a66dca3ea1da2d586f7bb89e68de
t8822_000 <t882200@gmail.com>
2018-09-28 00:37:45 +0300

    Coding React form with Formik and Ant Design

--- packages/web/src/modules/register/ui/RegisterView.tsx
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -1,18 +1,27 @@
 import * as React from 'react';
 import { Form, Icon, Input, Button } from 'antd';
+import { withFormik, FormikErrors, FormikProps } from 'formik';
 
 const FormItem = Form.Item;
 
-export class RegisterView extends React.PureComponent {
+interface FormValues {
+  email: string;
+  password: string;
+}
+interface Props {
+  submit: (values: FormValues) => Promise<FormikErrors<FormValues> | null>;
+}
+class C extends React.PureComponent<FormikProps<FormValues> & Props> {
   render() {
+    const {values, handleChange, handleBlur, handleSubmit} = this.props;
     return (
-      <div style={{display:'flex'}}>
+      <form style={{display:'flex'}} onSubmit={handleSubmit}>
         <div style={{width:400, margin: 'auto'}}>
           <FormItem>
-              <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+              <Input name="email" prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Email" value={values.email} onChange={handleChange} onBlur={handleBlur} />
           </FormItem>
           <FormItem>
-              <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />
+              <Input name="password" prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" value={values.password} onChange={handleChange} onBlur={handleBlur} />
           </FormItem>
           <FormItem>
             <a className="login-form-forgot" href="">Forgot password</a>
@@ -26,8 +35,21 @@ export class RegisterView extends React.PureComponent {
             Or <a href="">Log in now!</a>
           </FormItem>
         </div>
-      </div>
+      </form>
     );
   }
 
 }
+
+export const RegisterView = withFormik<Props, FormValues>({
+  mapPropsToValues: (props) => ({
+    email:"",
+    password: ""
+  }),
+  handleSubmit: async (values, {props, setErrors})=>{
+    const errors = await props.submit(values);
+    if(errors){
+      setErrors(errors);
+    }
+  }
+})(C);
